e7bcc57ccf7d25263324287d5f32b50f3fff46b1
ericlent <ericquaresma@hotmail.com>
2024-08-01 17:30:40 -0300

    Finalizando as interfaces

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -21,7 +21,7 @@ export function Home() {
                 renderItem={({ item }) => (
                     <Group
                         name={item}
-                        isActive={groupSelected === item}
+                        isActive={groupSelected.toLowerCase() === item.toLowerCase()}
                         onPress={() => setGroupSelected(item)}
 
                     />
